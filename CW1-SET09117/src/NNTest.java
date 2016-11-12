c37f5477cb91a2cca4ba256338d075515506a9b8
Danayal Iftikhar <danayal_94@hotmail.com>
2016-11-12 03:31:43 +0000

    Minor changes and added comments

--- CW1-SET09117/src/NNTest.java
+++ CW1-SET09117/src/NNTest.java
@@ -4,21 +4,14 @@ import java.util.ArrayList;
 public class NNTest {
 
 	public static void main(String[] args) {
-		long start = System.nanoTime();
         NearestNeighbour near = new NearestNeighbour();
         
-        ArrayList<Point2D> Cities = new ArrayList<Point2D>();
+        ArrayList<Point2D> cities = new ArrayList<Point2D>();
         
-        double distance = 0;
         
-        Cities = TSPLib.loadTSPLib("Users/danayaliftikhar/Desktop/TSP/rl304.tsp");
         
-        distance = near.getDistance(Cities);
+        cities = TSPLib.loadTSPLib("Users/danayaliftikhar/Desktop/TSP/rl304.tsp");
         
-        System.out.println("Total time taken: " + (System.nanoTime() - start) / 1000 + "ms");
-        
-        System.out.println("The total distance is " + distance);
-
 	}
 
 }
--- CW1-SET09117/src/NearestNeighbour.java
+++ CW1-SET09117/src/NearestNeighbour.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class NearestNeighbour {
 	
-	public static ArrayList<Point2D> getRoute(ArrayList<Point2D> cities){
+	public static double routeTaken(ArrayList<Point2D> cities ){ //array of 2D points which will return a result of the route taken
 	
 	long startTime = System.currentTimeMillis(); //log the start time of the algorithm
 	
@@ -17,24 +17,38 @@ public class NearestNeighbour {
 	
 	while (cities.size() > 0){ //repeat until all cities have been added
 		Point2D closest = null;
-		result.add(currentCity);
-		double distance = Double.POSITIVE_INFINITY;
+		result.add(currentCity); //add current city to the result
+		double distance = Double.POSITIVE_INFINITY; //find the closest city to the current city and add it
 		
 		
 		for (Point2D cityPossible : listCities) {
-			if(currentCity.distance(cityPossible) < distance) {
-				closest = cityPossible;
-				distance = currentCity.distance(cityPossible);
-				
-			}
-		}
-	}
+			if(getDistance(currentCity, cityPossible) < distance) {
+				closest = currentCity;
+				distance = getDistance(currentCity, cityPossible);
+			} //end if
+			
+		} //end for loop
+		
+		cities.remove(closest); //remove the closest city so it isn't checked again
+		currentCity = closest; //set next closest city to the current city
+		
+	} //end while loop
+	
+	long endTime = System.currentTimeMillis(); //log the end time of the algorithm
+	long totalTime = endTime - startTime; //calculate the total runtime of the algorithm
+	System.out.println("The total runtime is " + totalTime + " ms"); //displays the total runtime in milliseconds in the console
 	return result;
 	}
 
-	public double getDistance(ArrayList<Point2D> cities) {
-		
+	private static double getDistance(Point2D currentCity, Point2D cityPossible) {
+		ArrayList<Point2D> cities = cities;
 		double distance = 0;
 		return 0;
 	}
+
+//	public double getDistance(ArrayList<Point2D> cities) { //gets the distance of the route taken
+//		
+//		double distance = 0;
+//		return 0;
+//	}
 }
